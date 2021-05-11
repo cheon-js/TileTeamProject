4f94d9a2241072351f17b31df67dd55ddc4fcac7
한슬기 <hanseulgi06@naver.com>
2021-05-12 01:11:48 +0900

    구글 맵 경도,위도

--- app/src/main/java/com/example/tileteamproject/MainActivity.java
+++ app/src/main/java/com/example/tileteamproject/MainActivity.java
@@ -4,19 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference test = database.getReference("test");
         test.setValue("test");
+    }
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        LatLng State = new LatLng(35.143476741986596, 129.03430537670374);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(State);
+
+
+
+        //markerOptions.title("서울");
+        //markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+
+
+        // 기존에 사용하던 다음 2줄은 문제가 있습니다.
+
+        // CameraUpdateFactory.zoomTo가 오동작하네요.
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(State));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(State, 15));
+
+
     }
 }

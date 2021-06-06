8bd96246a86664e678b7cb489e7b57e72e8440e3
deu3332 <saver427@naver.com>
2021-06-06 22:39:47 +0900

    지도 경로 합침 버전 자르파일 추가

---
+++ app/src/main/java/com/example/tileteamproject/board.java
@@ -0,0 +1,12 @@
+package com.example.tileteamproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class board  extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board);
+    }
+}
